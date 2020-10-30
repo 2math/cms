6c0c4b1847aa159bed2b1933ae594dae81c37516
Galeen <g.blagoev@futurist-labs.com>
2020-10-30 17:40:15 +0200

    users, no actions

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -56,9 +56,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "331e799d17ebf8440f58eb168e4cbc6f",
-"/": "331e799d17ebf8440f58eb168e4cbc6f",
-"main.dart.js": "69b608b661f9b71a8a2a431365465dbb",
+"index.html": "3532029ce56b840766e890b5592f57fd",
+"/": "3532029ce56b840766e890b5592f57fd",
+"main.dart.js": "882233806ae70bf1af43a79357323a79",
 "manifest.json": "dda7ef12f2f3dc26751796c9ca2b5160",
 "version.json": "d1305142737e15f3cb85c76faf993f68"
 };
